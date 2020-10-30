eff23da3b859972a0368ecb44954fbdd033144ad
axel launay <axellaunay5@gmail.com>
2020-10-30 15:10:00 +0100

    reponsifPageAticleFunctionAjoutPannier

--- ScriptPannier.js
+++ ScriptPannier.js
@@ -1,21 +1,81 @@
-//validation des champ du formulaire 
-var inscription = document.getElementById("inscription");
-var prenom = document.getElementById("Prenom");
-var prenomNull = document.getElementById("prenomNull");
-var prenomValidation = /^[a-zA-Zéèîï] [a-zéèêàçîï] + ([-'\s] [a-zA-Zéèîï] [a-zéèêàçîï] +)?/;
-inscription.addEventListener('click',f_valid);
 
-function f_valid(e){
-    console.log(e)
-    if (prenom.validity.valueMissing){
-        e.preventDefault();
-        prenomNull.textContent = "Prenom manquant";
-        prenomNull.style.color = "red";
-    }else if (prenomValidation.test(prenom.value) == false) {
-        event.preventDefault();
-        prenomNull.textContent = 'format incorrect';
-        prenomNull.style.color = 'red';
-    }else{
+//var urlString = window.location.href
+//var url = new URL(urlString);
+var cart = JSON.parse(localStorage.getItem('cart'));
+var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            var cameras = JSON.parse(this.responseText);
+            console.log(cameras);
+            cameras.forEach(camera => {
 
-    }
-}
+                //console.log(camera.name)
+                
+                cart.forEach(element=>{
+                    if(element.objectID == camera._id){
+                    console.log(element)
+
+                    var container = document.getElementById("container");
+                        var name = document.createElement("p");
+                        name.className = "cameraName";
+                        name.textContent = camera.name;
+                        container.appendChild(name);
+
+                        
+                        var imageUrl = document.createElement("img");
+                        imageUrl.className = "imageCamera";
+                        imageUrl.setAttribute ("src",camera.imageUrl);
+                        container.appendChild(imageUrl);
+
+                        var description = document.createElement("p");
+                        description.className = "articleDescription";
+                        description.textContent = " " + camera.description;
+                        container.appendChild(description);
+        
+                        var lense = document.createElement("p");
+                        lense.className = "lentilChoisi";
+                        lense.textContent = " " + camera.lenses;
+                        container.appendChild(lense);
+        
+                        var price = document.createElement("p");
+                        price.className = "articlePrice";
+                        price.textContent = " " + (camera.price) /100 +" €";
+                        container.appendChild(price);
+
+                        
+                    
+                }
+                })
+
+                /*
+
+                var positionName = document.getElementById("positionName");
+                var name = document.createElement("p");
+                name.className = "cameraName";
+                name.textContent = camera.name;
+                positionName.appendChild(name);
+
+                var positionParam = document.getElementById("positionParam")
+                var description = document.createElement("p");
+                description.className = "articleDescription";
+                description.textContent = " " + camera.description;
+                positionParam.appendChild(description);
+
+                var lense = document.createElement("p");
+                lense.className = "lentilChoisi";
+                lense.textContent = " " + camera.lenses;
+                positionParam.appendChild(lense);
+
+                var formulaire = document.getElementById("formulaire");
+                var price = document.createElement("p");
+                price.className = "articlePrice";
+                price.textContent = " " + (camera.price) /100 +" €";
+                formulaire.appendChild(price);*/
+                
+            });
+
+        }
+ }
+
+    request.open ("GET" , "http://localhost:3000/api/cameras/");
+    request.send();
